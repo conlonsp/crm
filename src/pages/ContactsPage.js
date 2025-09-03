7953a8011a6c507ebb17ee37294bd818391ea292
samuelconlon <conlonsp@yahoo.com>
2025-09-03 13:02:27 -0600

    import BrowserRouter in index.js and populate App and pages with basic jsx

---
+++ src/pages/ContactsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactsPage() {
+  return (
+    <div>
+      <h1>Contacts Page</h1>
+    </div>
+  )
+}
+
+export default ContactsPage;
---
+++ src/pages/DealsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DealsPage() {
+  return (
+    <div>
+      <h1>Deals Page</h1>
+    </div>
+  )
+}
+
+export default DealsPage;
