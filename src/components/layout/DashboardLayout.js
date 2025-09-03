83ad8667b9656b29077b07d86eb6918ae2fc309b
samuelconlon <conlonsp@yahoo.com>
2025-09-03 13:26:56 -0600

    populate DashboardLayout

--- src/components/layout/DashboardLayout.js
+++ src/components/layout/DashboardLayout.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import Breadcrumbs from "./BreadCrumbs";
+import { Outlet } from "react-router-dom"
 
 function DashboardLayout() {
   return (
-    <div>
-      <h1>DashboardLayout</h1>
+    <div className="app-layout flex flex-col h-screen">
+      <Navbar />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <div className="flex flex-col flex-1 p-4 overflow-y-auto">
+          <Breadcrumbs />
+          <Outlet /> {/* This is where the active page will render */}
+          <Footer />
+        </div>
+      </div>
     </div>
   )
 }
