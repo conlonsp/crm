1ec1405b010025287caec7311d80ec4f5c7a4bd6
samuelconlon <conlonsp@yahoo.com>
2025-09-03 17:41:58 -0600

    add Back to Dashboard shortcut for LoginPage; removed main-content className

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom"
 
 function LoginPage() {
   return (
-    <div className="main-content">
+    <div className="login-page">
       <h1>Login Page</h1>
+      <NavLink
+        to="/"
+        exact
+      >
+        Back to Dashboard
+      </NavLink>
     </div>
   )
 }
