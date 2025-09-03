332d6f268429dcad9d1be3c570b57f9e134ad547
samuelconlon <conlonsp@yahoo.com>
2025-09-03 13:20:15 -0600

    add files in layout folder for Breadcrumbs, DashboardLayout, Footer, Navbar, and Sidebar. Added placeholder code in each file

---
+++ src/components/layout/Breadcrumbs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Breadcrumbs() {
+  return (
+    <div>
+      <h1>Breadcrumbs</h1>
+    </div>
+  )
+}
+
+export default Breadcrumbs;
---
+++ src/components/layout/DashboardLayout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DashboardLayout() {
+  return (
+    <div>
+      <h1>DashboardLayout</h1>
+    </div>
+  )
+}
+
+export default DashboardLayout;
