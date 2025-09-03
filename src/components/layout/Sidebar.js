9c00f6af727341d2d67e4f8e31fd3765564c03a0
samuelconlon <conlonsp@yahoo.com>
2025-09-03 16:38:29 -0600

    add NavLinks for pages in Sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,59 @@
 import React from "react";
+import ReactDOM from "react-dom"
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <h1>Sidebar</h1>
+      <NavLink
+        to="/"
+        exact
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        to="/companies"
+        exact
+      >
+        Companies
+      </NavLink>
+      <NavLink
+        to="/contacts"
+        exact
+      >
+        Contacts
+      </NavLink>
+      <NavLink
+        to="/deals"
+        exact
+      >
+        Deals
+      </NavLink>
+      <NavLink
+        to="/tasks"
+        exact
+      >
+        Tasks
+      </NavLink>
+      <NavLink
+        to="/reports"
+        exact
+      >
+        Reports
+      </NavLink>
+      <NavLink
+        to="/settings"
+        exact
+      >
+        Settings
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+      >
+        Login
+      </NavLink>
     </div>
   )
 }
